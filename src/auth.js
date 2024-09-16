e572dd7ad9c6393f5f3dd52fce0523a3ee685805
mouhcine <w.mouhcine05@gmail.com>
2024-09-16 23:43:26 +0100

    delete the last code been added

--- src/auth.js
+++ src/auth.js
@@ -9,15 +9,6 @@ async function authenticateUser(email, password) {
         const result = await pool.query('SELECT * FROM test_list_revendeur WHERE email = $1', [email]);
         //console.log('Database result:', result.rows); // Log database result
         
-        pool.query('SELECT * FROM users LIMIT 1', (error, results) => {
-            if (error) {
-                console.error('Error querying the database:', error);
-            } else {
-                console.log('Database connected, users:', results.rows);
-            }
-        });
-        
-
         if (result.rows.length > 0) {
             const user = result.rows[0];
             //console.log('User found:', user); // Log the user found
